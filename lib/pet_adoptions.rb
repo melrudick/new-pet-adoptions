e4a41366512da534955bea1ad75fdb1086d60c83
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-31 20:45:05 +0000

    initial commit

---
+++ lib/pet_adoptions.rb
@@ -0,0 +1,10 @@
+require "pet_adoptions/version"
+require 'rest-client'
+require 'json'
+require 'pry'
+
+require "pet_adoptions/api"
+
+module PetAdoptions
+  # Your code goes here...
+end
---
+++ lib/pet_adoptions/api.rb
@@ -0,0 +1,12 @@
+class PetAdoptions::API
+  attr_accessor :pets
+
+  def self.pet_list
+    pet = RestClient.get("http://api.petfinder.com/pet.getRandom?format=json?key=5155468caa12a7461aa6741c8b7a35f9")
+    binding.pry
+    @pet_list = JSON.parse(pets)
+    @pet_list["results"].each do |animal|
+      PetAdoptions::Pet.new_from_json(animal)
+    end
+  end
+end
