3cb8ff70a81fbc083ceefe1505a4f815e9688df6
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-31 21:25:57 +0000

    added new files

--- lib/pet_adoptions.rb
+++ lib/pet_adoptions.rb
@@ -1,10 +1,13 @@
-require "pet_adoptions/version"
+
 require 'rest-client'
 require 'json'
-require 'pry'
+# require 'pry'
 
-require "pet_adoptions/api"
+require_relative "./pet_adoptions/api"
+require_relative "./pet_adoptions/version"
+require_relative "./pet_adoptions/cli"
+require_relative "./pet_adoptions/pets"
 
-module PetAdoptions
-  # Your code goes here...
-end
+# module PetAdoptions
+#   # Your code goes here...
+# end
