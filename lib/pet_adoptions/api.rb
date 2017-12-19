9f3d56216c428fbac301e122a30b00687923b86e
Mel Rudick <melissarudick@MacBook-Air.local>
2017-12-18 23:52:03 -0600

    add changes from 18.12.17

--- lib/pet_adoptions/api.rb
+++ lib/pet_adoptions/api.rb
@@ -1,34 +1,35 @@
 class PetAdoptions::API
 
   def self.get_breed_list
-    breed = RestClient.get("http://api.petfinder.com/breed.list?format=json&key=5155468caa12a7461aa6741c8b7a35f9&animal=dog")
-    @@breed_list = JSON.parse(breed)["petfinder"]["breeds"]["breed"]
-    @@breed_list.each.with_index(1) do |index, dog_breed|
-      puts "#{dog_breed}. #{index["$t"]}"
-    end
-  end
+   breed = RestClient.get("http://api.petfinder.com/breed.list?format=json&key=5155468caa12a7461aa6741c8b7a35f9&animal=dog")
+   @@breed_list = JSON.parse(breed)["petfinder"]["breeds"]["breed"]
+   @@breed_list.each.with_index(1) do |dog_breed, index|
+     puts "#{index}. #{dog_breed["$t"]}"
+   end
+ end
 
-  def self.breed_list
-    @@breed_list
-  end
+ def self.breed_list
+   @@breed_list
+ end
 
-  def self.pet_list
-    @@pet_list
-  end
-
-  def self.get_pet_list(zip_code, breed_selection)
-    breed = URI::encode(breed_selection)
-    url = "http://api.petfinder.com/pet.find?format=json&key=5155468caa12a7461aa6741c8b7a35f9&animal=dog&location=#{zip_code}&breed=#{breed}"
-    pet_list = RestClient.get(url)
-    @pet_list = JSON.parse(pet_list)["petfinder"]["pets"]["pet"]
-    if @pet_list != nil
-    @pet_list.collect do |dog|
-      PetAdoptions::Pets.new_from_json(dog)
-    end
-    else
-      puts "No dogs are available of that breed."
-      puts "Please make another selection."
-      PetAdoptions::CLI.new.menu
-    end
-  end
+ def self.get_pet_list(zip_code, breed_selection)
+   breed = URI::encode(breed_selection)
+   url = "http://api.petfinder.com/pet.find?format=json&key=5155468caa12a7461aa6741c8b7a35f9&animal=dog&location=#{zip_code}&breed=#{breed}"
+   pet_list = RestClient.get(url)
+   @response = JSON.parse(pet_list)
+   if @response["petfinder"]["header"]["status"]["code"]["$t"] == "100"
+     @pet_list = @response["petfinder"]["pets"]["pet"]
+     if @pet_list != nil
+     @pet_list.collect do |dog|
+       PetAdoptions::Pets.new_from_json(dog)
+     end
+       return true
+     else
+       puts "No dogs are available of that breed."
+       puts "Please make another selection."
+     end
+   else
+     puts "Please enter a valid response:"
+   end
+ end
 end
--- lib/pet_adoptions/cli.rb
+++ lib/pet_adoptions/cli.rb
@@ -1,49 +1,57 @@
 class PetAdoptions::CLI
 
   def call
-    puts "Welcome to Pet Adoptions!!"
-    puts "Thousands of dogs need a forever home."
-    puts "Let's see who's looking for a home today:"
-    menu
-  end
+     puts "Welcome to Pet Adoptions!!"
+     puts "Thousands of dogs need a forever home."
+     puts "Let's see who's looking for a home today:"
+     menu
+   end
 
-  def menu
-    input = ""
-    puts "Please enter your zip code:"
-    zip = gets.strip.to_i
-    puts "Please choose the number of the breed you would like to see:"
-    PetAdoptions::API.get_breed_list
-    choice = gets.strip.to_i-1
-    breed = PetAdoptions::API.breed_list[choice]["$t"]
-    PetAdoptions::API.get_pet_list(zip, breed)
-    dog_list = PetAdoptions::Pets.all
-    list_all_dogs(dog_list, breed)
-    puts ""
-    puts "Would you like to see another breed? Enter Y or N:"
-    input = gets.strip.downcase
-    if input == "y"
-      menu
-    else
-      puts ""
-      puts "Thank you for searching!"
-      puts "Goodbye!"
-    end
-  end
+   def menu
+     sleep(2)
+     input = ""
+     PetAdoptions::API.get_breed_list
+     puts "Please choose the number of the breed you would like to see:"
+     choice = gets.strip.to_i-1
+     if !choice.between?(0,PetAdoptions::API.breed_list.size-1)
+       puts "Invalid choice"
+       menu
+     end
+     breed = PetAdoptions::API.breed_list[choice]["$t"]
+     puts "Please enter your zip code:"
+     zip = gets.strip
+     if PetAdoptions::API.get_pet_list(zip, breed).nil?
+       menu
+     end
+     dog_list = PetAdoptions::Pets.all
+     list_all_dogs(dog_list, breed)
+     puts ""
+     puts "Would you like to see another breed? Enter Y or N:"
+     input = gets.strip.downcase
+     if input == "y"
+       menu
+     else
+       puts ""
+       puts "Thank you for searching!"
+       puts "Goodbye!"
+       exit
+     end
+   end
 
-  def list_all_dogs(dog_list, breed)
-    dog_list.each do |dog|
-      dog_specs(dog, breed)
-    end
-  end
+   def list_all_dogs(dog_list, breed)
+     dog_list.each do |dog|
+       dog_specs(dog, breed)
+     end
+   end
 
-  def dog_specs(dog, breed)
-    puts "_____#{dog.name}_____"
-    puts "#{breed}"
-    puts "Sex: #{dog.sex}"
-    puts "Age: #{dog.age}"
-    if dog.description != nil
-      puts "About: #{dog.description}"
-    end
-    puts ""
+   def dog_specs(dog, breed)
+     puts "_____#{dog.name}_____"
+     puts "#{breed}"
+     puts "Sex: #{dog.sex}"
+     puts "Age: #{dog.age}"
+     if dog.description != nil
+       puts "About: #{dog.description}"
+     end
+     puts ""
+   end
   end
-end
